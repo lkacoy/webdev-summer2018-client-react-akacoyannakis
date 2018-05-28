a99e5cb6e041cf5a195c58bd7f2cf38315a40a64
lkacoy <lkacoyannakis@gmail.com>
2018-05-28 16:55:58 -0400

    Add LessonTabs

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import CourseCard from '../components/CourseCard';
 import ModuleList from './ModuleList';
+import LessonTabs from './LessonTabs';
 
 class CourseManager extends Component {
     render() {
         return (
             <div className="container-fluid">
                 <h1>Course Manager</h1>
+                <LessonTabs/>
                 <ModuleList/>
                 <div className="card-deck">
                     <CourseCard/>
