b84078e08979260aff24204429564e8895a3461f
lkacoy <lkacoyannakis@gmail.com>
2018-05-28 17:47:51 -0400

    dynamically render from an array

--- src/containers/ModuleList2.js
+++ src/containers/ModuleList2.js
@@ -2,14 +2,36 @@ import React from 'react'
 import ModuleListItem from '../components/ModuleListItem'
 
 class ModuleList2 extends React.Component {
-    render() {
-        return (
-            <div>
-                <ModuleListItem title="React Module"/>
-                <ModuleListItem title="Redux Module"/>
-                <ModuleListItem title="React Native Module"/>
-            </div>
-        );
+
+        constructor() {
+            super();
+            this.state = {
+                modules: [
+                    {title: 'Module 1 - jQuery', id: 123},
+                    {title: 'Module 2 - React', id: 234},
+                    {title: 'Module 3 - Redux', id: 345},
+                    {title: 'Module 4 - Angular', id: 456},
+                    {title: 'Module 5 - Node.js', id: 567},
+                    {title: 'Module 6 - MongoDB', id: 678}
+                ]
+            };
+        }
+
+        renderListOfModules() {
+          let modules = this.state.modules
+              .map(function(module){
+            return <ModuleListItem
+                title={module.title} key={module.id}/>
+          });
+          return modules;
+        }
+
+        render() {
+            return (
+                <div>
+                {this.renderListOfModules()}
+                </div>
+            );
     }
 }
 
