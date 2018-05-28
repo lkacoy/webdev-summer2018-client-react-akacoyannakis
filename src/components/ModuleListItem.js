c885e4167d38e16dc258407dcf140bc37ded4c87
lkacoy <lkacoyannakis@gmail.com>
2018-05-28 16:18:49 -0400

    create ModuleList component

---
+++ src/components/ModuleListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../.../bootstrap/.../bootstrap.css';
+
+export default class ModuleListItem
+    extends React.Component {
+    render() {
+        return (
+            <li className="list-group-item">
+                Module 1
+            </li>
+        );
+    }
+}
