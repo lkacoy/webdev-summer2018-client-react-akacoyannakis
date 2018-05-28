c666e6d7278f2b453206ea829a44903819ff6435
lkacoy <lkacoyannakis@gmail.com>
2018-05-28 16:07:43 -0400

    implement coursemanager

---
+++ src/containers/CourseManager.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CourseManager extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Course Manager</h1>
+            </div>
+        )
+    }
+}
+
+export default CourseManager;
