a761d2d8419a10db6f2df64d259529cf3f3cc304
lkacoy <lkacoyannakis@gmail.com>
2018-05-28 16:59:52 -0400

    add ModuleList and LessonTabs to the CourseEditor

--- src/containers/CourseEditor.js
+++ src/containers/CourseEditor.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import ModuleList from './ModuleList';
+import LessonTabs from './LessonTabs';
 
 export default class CourseEditor extends React.Component {
     render() {
         return (
             <div className="row">
              <div className="col-4">
-               <h2>Left 1/3</h2>
+                <ModuleList/>
              </div>
              <div className="col-8">
-               <h2>Right 2/3</h2>
+                <LessonTabs/>
              </div>
             </div>
         )
