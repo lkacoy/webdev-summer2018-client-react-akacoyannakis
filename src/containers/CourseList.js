0bf505a8eeaa971269367dfede98c55d9923e8ac
lkacoy <lkacoyannakis@gmail.com>
2018-05-28 21:55:01 -0400

    make CourseRow configurable to read in the course titles

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -32,7 +32,7 @@ class CourseList extends React.Component {
    renderCourseRows() {
       let courses = this.state.courses.map(
         function (course) {
-            return <CourseRow/>
+            return <CourseRow course={course}/>
         }
       )
       return (
