6928a9bb6296d3499eb728b88694d2b428c6c63a
lkacoy <lkacoyannakis@gmail.com>
2018-06-01 20:38:31 -0400

    fix spacing between icons

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -12,7 +12,7 @@ export default class ModuleListItem
             <li className="list-group-item">
                 Module {this.props.module.id} - {this.props.module.title}
                 <span className="float-right">
-                    <span onClick={() =>
+                    <span className="mr-2" onClick={() =>
                     {this.props.delete(this.props.course.id, this.props.module.id)}}><i className="fa fa-trash"></i></span>
                     <span onClick={() =>
                     {this.props.update(this.props.course.id, this.props.module.id)}}><i className="fa fa-pencil"></i></span>
