645a70a046310351bbe364b9616c48e6ca80ead5
lkacoy <lkacoyannakis@gmail.com>
2018-06-03 18:59:40 -0400

    update form rendering for add form

--- src/components/LessonFormAdd.js
+++ src/components/LessonFormAdd.js
@@ -5,26 +5,62 @@ export default class LessonFormAdd extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lesson: {id: '', title: ''}
+            lesson: {id: '', title: '', topics: []},
+            topic: ''
         };
         this.titleChanged = this.titleChanged.bind(this);
+        this.topicChanged = this.topicChanged.bind(this);
     }
 
     titleChanged(event) {
         this.setState({lesson: {title: event.target.value}});
     }
 
+    topicChanged(event){
+        this.setState({topic: event.target.value});
+        console.log(this.state);
+    }
+
     render() {
-        return <form className="mt-4">
-           Lesson Title:  <input  className="form-control mb-8"
-                                  onChange={this.titleChanged}
-                                  placeholder="title"
-                                  value={this.state.lesson.title}/>
-            <button className= "btn btn-primary btn-block mt-4" type="submit"
-                    onClick={this.createLesson}>
+        return <div className="form-control mt-4">
+            <h1 className="form-check-label mb-5">Add New Lesson</h1>
+            <div className="form-group">
+                <label htmlFor="lessonTitle">Lesson Title</label>
+                <input type="text" className="form-control mb-8" onChange={this.titleChanged} id="lessonTitle" placeholder="Lesson Title" value={this.state.lesson.title}/>
+            </div>
+{/*            <div className="form-group">
+                <label htmlFor="lessonTopics">Topics</label>
+                <div className="form-inline form-control mb-5">
+                    <input
+                        type="text"
+                        onChange={this.topicChanged}
+                        className="form-control"
+                        placeholder="Topic"
+                        value={this.state.topic}
+                    />
+                    <span className="ml-2" onClick={this.handleRemoveTopics}><i className="fa fa-trash"></i></span>
+                    <span className="ml-2" onClick={this.handleAddTopics()}><i className="fa fa-plus"></i></span>
+                </div>
+            </div>*/}
+            <button className= "btn btn-primary btn-block mt-4"
+                    onClick={this.props.createLesson(this.state.lesson)}>
                 <i className="fa fa-plus"></i>
             </button>
-        </form>
+        </div>
+    }
+
+    handleRemoveTopics() {
+        var topics = this.state.lesson.topics;
+        topics[this.props.lesson.id] = this.state.lesson.topic;
+        this.setState({
+            lesson: {topics: topics}
+        });
+    }
+
+    handleAddTopics() {
+        console.log("add topic");
+        console.log(this.state);
+        this.state.lesson.topics.push(this.state.lesson.topics);
     }
 
 }
