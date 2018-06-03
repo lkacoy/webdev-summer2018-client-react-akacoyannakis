fa47179645bc08ea6e13f6378d718d23b699bf8f
lkacoy <lkacoyannakis@gmail.com>
2018-06-02 22:51:40 -0400

    Switch to link instead of using an onclick event

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default class ModuleListItem
     extends React.Component {
 
     constructor(props) {
         super(props);
+        console.debug(this.props.isSelected);
     }
-//activeClassName="cm-active-link" to={`/course/${this.props.courseId}/module/${this.props.module.id}`}
     render() {
         return (
-            <NavLink activeClassName="cm-active-link" to={`/course/${this.props.courseId}/module/${this.props.module.id}`}>
-                <li className={this.props.module.active ? 'list-group-item active' : 'list-group-item'} onClick={() =>
-                {this.props.findModuleById(this.props.module.id)}}>
-                    {this.props.module.title}
-                    <span className="float-right">
+            <Link to={`/course/${this.props.courseId}/module/${this.props.module.id}`}>
+            <li className="list-group-item">
+                {this.props.module.title}
+                <span className="float-right">
                         <span className="mr-2" onClick={() =>
                         {this.props.delete(this.props.courseId, this.props.module.id)}}><i className="fa fa-trash"></i></span>
                         <span onClick={() =>
                         {this.props.update(this.props.courseId, this.props.module.id)}}><i className="fa fa-pencil"></i></span>
                     </span>
-                </li>
-            </NavLink>
+            </li>
+            </Link>
         );
     }
 }
