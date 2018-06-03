21dc8e989eaa6df4ce30afcb92e8f51f9a902dcf
lkacoy <lkacoyannakis@gmail.com>
2018-06-02 20:35:27 -0400

    update title of list items

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 export default class ModuleListItem
     extends React.Component {
@@ -6,12 +7,13 @@ export default class ModuleListItem
     constructor(props) {
         super(props);
     }
-
+//activeClassName="cm-active-link" to={`/course/${this.props.courseId}/module/${this.props.module.id}`}
     render() {
         return (
-                <li className="list-group-item" onClick={() =>
+            <NavLink activeClassName="cm-active-link" to={`/course/${this.props.courseId}/module/${this.props.module.id}`}>
+                <li className={this.props.module.active ? 'list-group-item active' : 'list-group-item'} onClick={() =>
                 {this.props.findModuleById(this.props.module.id)}}>
-                    <a href="">Module {this.props.module.id} - {this.props.module.title}</a>
+                    {this.props.module.title}
                     <span className="float-right">
                         <span className="mr-2" onClick={() =>
                         {this.props.delete(this.props.courseId, this.props.module.id)}}><i className="fa fa-trash"></i></span>
@@ -19,6 +21,7 @@ export default class ModuleListItem
                         {this.props.update(this.props.courseId, this.props.module.id)}}><i className="fa fa-pencil"></i></span>
                     </span>
                 </li>
+            </NavLink>
         );
     }
 }
