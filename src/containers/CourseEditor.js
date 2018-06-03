47af57148649215db001dac1bb58eaf5671d0d3c
lkacoy <lkacoyannakis@gmail.com>
2018-06-02 21:55:07 -0400

    add moduleId to Course Editor state

--- src/containers/CourseEditor.js
+++ src/containers/CourseEditor.js
@@ -7,19 +7,25 @@ export default class CourseEditor extends React.Component {
     constructor(props) {
         super(props);
         this.selectCourse = this.selectCourse.bind(this);
-        this.state = {courseId: ''};
+        this.state = {courseId: '', moduleId: ''};
     }
 
     componentDidMount() {
         this.selectCourse(this.props.match.params.courseId);
+        this.selectModule(this.props.match.params.moduleId);
     }
 
     componentWillReceiveProps(newProps) {
         this.selectCourse(newProps.match.params.courseId);
+        this.selectModule(newProps.match.params.moduleId);
     }
 
     selectCourse(courseId) {
         this.setState({courseId: courseId});
+    }
+
+    selectModule(moduleId) {
+        this.setState({moduleId: moduleId});
     }
 
     render() {
@@ -30,7 +36,7 @@ export default class CourseEditor extends React.Component {
                         <ModuleList courseId={this.state.courseId}/>
                     </div>
                     <div className="col-8">
-                        <LessonTabs/>
+                        <LessonTabs courseId={this.state.courseId} moduleId={this.state.moduleId}/>
                     </div>
                 </div>
             </div>
