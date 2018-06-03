532a0483a947ce9de15f366768f2c1fde8fece96
lkacoy <lkacoyannakis@gmail.com>
2018-06-02 22:52:01 -0400

    implement lessonstabs to be dynamic

--- src/containers/LessonTabs.js
+++ src/containers/LessonTabs.js
@@ -1,14 +1,56 @@
 import React from 'react';
+import LessonService from "../services/LessonService";
 
 export default class LessonTabs extends React.Component {
-     render() {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            courseId: '',
+            module: { title: '', id: '' },
+            lessons: []
+        }
+        this.lessonService = LessonService.instance;
+    }
+
+    componentDidMount() {
+        this.setCourseId(this.props.courseId);
+        this.setModuleId(this.props.module);
+    }
+    componentWillReceiveProps(newProps){
+        this.findAllLessonsForModule(newProps.courseId, newProps.module);
+        console.log(newProps.module);
+    }
+
+    render() {
       return(
           <ul className="nav nav-tabs">
-               <li className="nav-item"><a className="nav-link active"
-                    href="#">Active Tab</a></li>
+              {this.renderLessons()}
+               {/*<li className="nav-item"><a className="nav-link active"
+                    href="">Active Tab</a></li>
                <li className="nav-item"><a className="nav-link"
-                    href="#">Another Tab</a></li>
+                    href="">Another Tab</a></li>*/}
           </ul>
       );
      }
+
+     renderLessons() {
+         let lessons = this.state.lessons.map(lesson =>{
+             return <li className="nav-item"><a className="nav-link active"
+                                                href="">{lesson.title}</a></li>
+         });
+         return (lessons);
+     }
+
+     findAllLessonsForModule(courseId, moduleId) {
+        this.lessonService.findAllLessonsForModule(courseId, moduleId);
+     }
+
+    setCourseId(courseId) {
+        this.setState({courseId: courseId});
+    }
+
+    setModuleId(module) {
+        this.setState({module: module});
+    }
 }
