95bc767cf800542737f18ae45bb743a6fbd06692
lkacoy <lkacoyannakis@gmail.com>
2018-06-03 15:28:44 -0400

    update spacing on lessonformadd

--- src/components/LessonFormAdd.js
+++ src/components/LessonFormAdd.js
@@ -16,10 +16,14 @@ export default class LessonFormAdd extends React.Component {
 
     render() {
         return <form className="mt-4">
-           Lesson Title:  <input  className="form-control"
+           Lesson Title:  <input  className="form-control mb-8"
                                   onChange={this.titleChanged}
                                   placeholder="title"
                                   value={this.state.lesson.title}/>
+            <button className= "btn btn-primary btn-block mt-4" type="submit"
+                    onClick={this.createLesson}>
+                <i className="fa fa-plus"></i>
+            </button>
         </form>
     }
 
