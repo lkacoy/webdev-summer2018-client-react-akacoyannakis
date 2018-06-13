82847dec7b82ffc9a5a242075d40705893af6702
lkacoy <lkacoyannakis@gmail.com>
2018-06-12 21:50:12 -0400

    add findAllTopicsForLesson to topicservice

---
+++ src/services/TopicService.js
@@ -0,0 +1,20 @@
+const TOPIC_API_URL =
+    'http://localhost:8080/api/lesson/{LID}/topic';
+
+let _singleton = Symbol();
+export default class ModuleService {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Singleton!!!');
+    }
+
+    findAllTopicsForLesson(lessonId) {
+        return fetch(
+            TOPIC_API_URL
+                .replace('LID', lessonId))
+            .then(function (response) {
+                return response.json();
+            })
+    }
+
+}
